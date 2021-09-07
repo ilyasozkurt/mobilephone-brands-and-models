650a51425f27ffa753b48c50803e8fa5b488d6c9
ozkurtilyas <ilyas.ozkurt@beyn.com.tr>
2021-09-07 14:13:37 +0300

    Brand model created.

---
+++ scrapper/app/Models/Brand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Brand extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * @var string
+     */
+    protected $table = 'brands';
+
+    /**
+     * Returns devices of brand.
+     *
+     * @return HasMany
+     */
+    public function devices(): HasMany
+    {
+        return $this->hasMany('App\Models\Device');
+    }
+}
