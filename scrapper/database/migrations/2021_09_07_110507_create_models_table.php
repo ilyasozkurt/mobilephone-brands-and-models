723b39d6c16b6490df5fee4e1657f56d4a9d3b73
ozkurtilyas <ilyas.ozkurt@beyn.com.tr>
2021-09-07 14:07:56 +0300

    Models table migration created.

---
+++ scrapper/database/migrations/2021_09_07_110507_create_models_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('models', function (Blueprint $table) {
+            $table->id();
+            $table->integer('brand_id')->index('brand_id');
+            $table->string('name')->index('name');
+            $table->string('picture');
+            $table->text('specifications');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('models');
+    }
+}
